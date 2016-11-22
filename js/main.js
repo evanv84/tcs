02d651e741247de31bd9bb62ed8d28e6abbd8b19
evanv84 <impureivan@gmail.com>
2016-11-22 16:50:43 +0400

    constructor added

--- js/main.js
+++ js/main.js
@@ -79,4 +79,466 @@ $(document).ready(function() {
 	//.addIndicators()
 	.addTo(controller);
 
+
+	//CONSTRUCTOR CONFIG //////////////////////////////////
+	var type = $('#type').val();
+	var product = $('#product-type').val();
+	var rashod = $('#rashod').val();
+	var rec = $('#recorder').val();
+
+	$('#product1').hide();
+	$('#product2').hide();
+	$('#product3').hide();
+	$('#product4').hide();
+	$('#product5').hide();
+	$('#product6').hide();
+	$('#product7').hide();
+	$('#product8').hide();
+	$('#product9').hide();
+	$('#rashod1').hide();
+	$('#rashod2').hide();
+	$('#rashod3').hide();
+	$('#rashod4').hide();
+	$('#rashod5').hide();
+	$('#rashod6').hide();
+	$('#rashod7').hide();
+	$('#rashod8').hide();
+	$('#rashod9').hide();
+	$('#rashod10').hide();
+	$('#recorder1').hide();
+	$('#recorder2').hide();
+	$('#recorder3').hide();
+	$('#recorder4').hide();
+	$('#recorder5').hide();
+	$('#add4').prop('disabled', true);
+	$('#add5').prop('disabled', true);
+	$('#add6').prop('disabled', true);
+
+	
+	$('#type').change(function(event) {
+		var type = $('#type').val();
+		$('#product-type').val('def');
+		$('#rashod').val('def');
+		$('#recorder').val('def');
+
+		if (type == 'type1') {
+			$('#product1').show();
+			$('#product2').show();
+			$('#product3').show();
+			$('#product4').show();
+			$('#product5').show();
+			$('#product6').show();
+			$('#product7').show();
+			$('#product8').show();
+			$('#product9').show();
+		}
+
+		if (type == 'type2') {
+			$('#product1').show();
+			$('#product2').show();
+			$('#product3').show();
+			$('#product4').hide();
+			$('#product5').hide();
+			$('#product6').show();
+			$('#product7').show();
+			$('#product8').show();
+			$('#product9').show();
+		}
+	});
+		
+
+
+	$('#product-type').change(function(event) {
+		var type = $('#type').val();
+		var product = $('#product-type').val();
+		$('#rashod').val('def');
+		$('#recorder').val('def');
+
+		if (type == 'type1') {
+
+			if (product == 'product1') {
+				$('#rashod1').hide();
+				$('#rashod2').show();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').show();
+				$('#rashod6').hide();
+				$('#rashod7').show();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product2') {
+				$('#rashod1').hide();
+				$('#rashod2').show();
+				$('#rashod3').show();
+				$('#rashod4').show();
+				$('#rashod5').show();
+				$('#rashod6').show();
+				$('#rashod7').show();
+				$('#rashod8').show();
+				$('#rashod9').show();
+				$('#rashod10').show();
+			}
+
+			if (product == 'product3') {
+				$('#rashod1').hide();
+				$('#rashod2').hide();
+				$('#rashod3').show();
+				$('#rashod4').show();
+				$('#rashod5').show();
+				$('#rashod6').show();
+				$('#rashod7').show();
+				$('#rashod8').show();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product4') {
+				$('#rashod1').hide();
+				$('#rashod2').show();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').show();
+				$('#rashod6').hide();
+				$('#rashod7').show();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product5') {
+				$('#rashod1').hide();
+				$('#rashod2').show();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').show();
+				$('#rashod6').hide();
+				$('#rashod7').show();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product6') {
+				$('#rashod1').hide();
+				$('#rashod2').hide();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').show();
+				$('#rashod6').hide();
+				$('#rashod7').show();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product7') {
+				$('#rashod1').hide();
+				$('#rashod2').hide();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product8') {
+				$('#rashod1').hide();
+				$('#rashod2').hide();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+			if (product == 'product9') {
+				$('#rashod1').hide();
+				$('#rashod2').hide();
+				$('#rashod3').show();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+		}
+
+		if (type == 'type2') {
+
+			if (product == 'product1') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product2') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product3') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product4') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product5') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product6') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product7') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+
+			if (product == 'product8') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+			if (product == 'product9') {
+				$('#rashod1').show();
+				$('#rashod2').hide();
+				$('#rashod3').hide();
+				$('#rashod4').hide();
+				$('#rashod5').hide();
+				$('#rashod6').hide();
+				$('#rashod7').hide();
+				$('#rashod8').hide();
+				$('#rashod9').hide();
+				$('#rashod10').hide();
+			}
+		}
+	});
+
+	$('#rashod').change(function(event) {
+		var product = $('#product-type').val();
+		var rashod = $('#rashod').val();
+		$('#recorder').val('def');
+		$('#recorder1').show();
+		$('#recorder2').show();
+		$('#recorder3').show();
+		$('#recorder4').show();
+		$('#recorder5').show();
+		
+	});
+
+	$('#recorder').change(function(event) {
+		var rec = $('#recorder').val();
+
+		if ((rec == 'recorder2') || (rec == 'recorder3') || (rec == 'recorder4')) {
+			$('#add4').prop('disabled', true);
+			$('#add5').prop('disabled', false);
+
+			if (rec == 'recorder4') {
+				$('#add8').prop('checked', true);
+			}
+		}
+
+		if (rec == 'recorder1') {
+			$('#add4').prop('disabled', false);
+			$('#add5').prop('disabled', true);
+		}
+
+		if (rec == 'recorder5') {
+			$('#add4').prop('disabled', true);
+			$('#add5').prop('disabled', true);
+		}
+	});
+
+	$('#add1').click(function(event) {
+
+		if (($('#add1').is(':checked')) || ($('#add2').is(':checked')) || ($('#add9').is(':checked'))) {
+			$('#add6').prop('disabled', false);
+		}
+
+		else {
+			$('#add6').prop('disabled', true);
+		}
+	});
+
+	$('#add2').click(function(event) {
+
+		if (($('#add1').is(':checked')) || ($('#add2').is(':checked')) || ($('#add9').is(':checked'))) {
+			$('#add6').prop('disabled', false);
+		}
+
+		else {
+			$('#add6').prop('disabled', true);
+		}
+	});
+
+	$('#add9').click(function(event) {
+
+		if (($('#add1').is(':checked')) || ($('#add2').is(':checked')) || ($('#add9').is(':checked'))) {
+			$('#add6').prop('disabled', false);
+		}
+
+		else {
+			$('#add6').prop('disabled', true);
+		}
+	});
+
+
+	$('#calc').click(function(event) {
+		var type = $('#type').val();
+		var product = $('#product-type').val();
+		var rashod = $('#rashod').val();
+		var rec = $('#recorder').val();
+
+		if ($('#add1').is(':checked')) {
+			var add1 = 1;
+		}
+		else {
+			var add1 = 0;
+		}
+		if ($('#add2').is(':checked')) {
+			var add2 = 1;
+		}
+		else {
+			var add2 = 0;
+		}
+		if ($('#add3').is(':checked')) {
+			var add3 = 1;
+		}
+		else {
+			var add3 = 0;
+		}
+		if ($('#add4').is(':checked')) {
+			var add4 = 1;
+		}
+		else {
+			var add4 = 0;
+		}
+		if ($('#add5').is(':checked')) {
+			var add5 = 1;
+		}
+		else {
+			var add5 = 0;
+		}
+		if ($('#add6').is(':checked')) {
+			var add6 = 1;
+		}
+		else {
+			var add6 = 0;
+		}
+		if ($('#add7').is(':checked')) {
+			var add7 = 1;
+		}
+		else {
+			var add7 = 0;
+		}
+		if ($('#add8').is(':checked')) {
+			var add8 = 1;
+		}
+		else {
+			var add8 = 0;
+		}
+		if ($('#add9').is(':checked')) {
+			var add9 = 1;
+		}
+		else {
+			var add9 = 0;
+		}		
+		var img682 = "<img src='../img/682.jpg'>";
+		var img700 = "<img src='../img/700.jpg'>";
+
+		if (type == 'type1') {
+			$("div.result").empty()
+			$('.result').append(img700);
+		}
+		
+		if (type == 'type2') {
+			$("div.result").empty()
+			$('.result').append(img682);
+		}
+
+		return false;
+
+	});
+
+	// .END CONSTRUCTOR CONFIG /////////////////////////
+
 });
